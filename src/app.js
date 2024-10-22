b220348671d4fa1af8440a7a7cfb3bc07de0f3ba
Kaushik das <kaushikd207@gmail.com>
2024-10-22 15:20:35 +0530

    GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("Hello from Dashboard");
-// });
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Hello from test routing");
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello!");
+app.delete("/hello", (req, res) => {
+  res.send("deleted successfully");
+});
+app.post("/dashboard", (req, res) => {
+  res.send("Data has been successfully saved");
 });
 app.listen(3002, () => {
   console.log("Server is listening at port 3002..");
