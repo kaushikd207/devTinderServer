0910b6621ae8f816db0a78a88af7c01b839f3c70
Kaushik das <kaushikd207@gmail.com>
2024-10-25 17:57:51 +0530

    added first post api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signUp", async (req, res) => {
+  const user = new User({
+    firstName: "Raja",
+    lastName: "Das",
+    mail: "raja@gmail.com",
+  });
+  try {
+    await user.save();
+    res.send(" User added successfully");
+  } catch (err) {
+    res.status(400).send("User can not be added " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://kaushikd207:test123@devtinder.8a79v.mongodb.net/?retryWrites=true&w=majority&appName=devTinder/devTinder"
+    "mongodb+srv://kaushikd207:test123@devtinder.8a79v.mongodb.net/devTinder"
   );
 };
 
